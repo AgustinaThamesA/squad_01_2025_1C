3ae9ddad545fe26c05f357a46a08738d88d05825
axel-zielonka <azielonka@fi.uba.ar>
2025-06-09 13:13:47 -0300

    feat(tests unitarios)

---
+++ app/src/test/java/org/psa/model/RiesgoTest.java
@@ -0,0 +1,46 @@
+package src.test.java.org.psa.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.psa.model.Riesgo;
+
+public class RiesgoTest {
+    private static Riesgo riesgo;
+
+    @BeforeAll
+    public static void setUp() {
+        riesgo = new Riesgo("Riesgo test", Riesgo.Probabilidad.BAJA, Riesgo.Impacto.BAJO);
+    }
+
+    @Test
+    public void crearRiesgoTest() {
+        Assertions.assertNotNull(riesgo);
+        Assertions.assertEquals("Riesgo test", riesgo.getDescripcion());
+        Assertions.assertEquals(Riesgo.Probabilidad.BAJA, riesgo.getProbabilidad());
+        Assertions.assertEquals(Riesgo.Impacto.BAJO, riesgo.getImpacto());
+        Assertions.assertEquals(Riesgo.Estado.ACTIVO, riesgo.getEstado());
+    }
+
+    @Test
+    public void calcularNivelRiesgoTest() {
+        Assertions.assertEquals(1, riesgo.calcularNivelRiesgo());
+    }
+
+    @Test
+    public void calcularNivelRiesgoTest2() {
+        Riesgo riesgo2 = new Riesgo("Riesgo 2", Riesgo.Probabilidad.ALTA, Riesgo.Impacto.ALTO);
+        Assertions.assertEquals(9, riesgo2.calcularNivelRiesgo());
+    }
+
+    @Test
+    public void noEsRiesgoAltoTest() {
+        Assertions.assertFalse(riesgo.esRiesgoAlto());
+    }
+
+    @Test
+    public void esRiesgoAltoTest() {
+        Riesgo riesgo3 = new Riesgo("Riesgo 3", Riesgo.Probabilidad.ALTA, Riesgo.Impacto.MEDIO);
+        assert (riesgo3.esRiesgoAlto());
+    }
+}
